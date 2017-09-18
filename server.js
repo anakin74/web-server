8af35746fb2d83bb5187a302078aebb20e821c40
Ronald van den Bos <ronald_van_den_bos@nl.ibm.com>
2017-09-18 12:55:58 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if  (err) {
+      console.log('could not write to file');
+    }
+  });
+  next();
+});
+
+
+app.use((req, res) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance Page'
+  })
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express </h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMessage: 'Hello, welcome on my website',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+//route /bad simulate that something fails
+//send back json data with an error message property
+//test it
+ app.get('/bad', (req, res) => {
+   res.send({
+     errorMessage: 'Could not fullfill request'
+   });
+ });
+
+app.listen(3000, () =>{
+  console.log('Server is up on port 3000');
+});
